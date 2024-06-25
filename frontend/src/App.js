e799275bfa78af8e63f6c651a74f129d61b49fd7
e-nitram <sammy.jam.robinson@gmail.com>
2024-06-25 09:13:34 -0700

    feat: done frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/items')
+      .then(response => response.json())
+      .then(data => setItems(data));
+  }, []);
+
+   return (
+    <div>
+      <h1>Items</h1>
+      <ul>
+        {items.map(item => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ frontend/src/serviceWorker.js
@@ -0,0 +1,2 @@
+// This optional code is used to register a service worker.
+// By default, this project is configured to not use a service worker.
